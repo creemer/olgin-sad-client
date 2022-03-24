beab1353c71f645b267def5e95f0958cb17eb5b0
akolpakov <akolpakov@aligntech.com>
2022-03-24 13:23:54 +0300

    feat: change cors settings

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors')
 
 const app = express();
 
 const port = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV === 'production') {
+    app.use(cors())
     app.use(express.static('build'));
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
